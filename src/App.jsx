6af76049969e1a929f8a18b7c0d30f1328ea27a8
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-06 17:55:48 -0500

    agregando navbar y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Emoji from './pages/Emoji';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const footerinfo = {
+    nombre: "© Kruger Corp. 2022. Transforming Enterprises, Boosting Lives.",
+  };
   return (
-    <div className="App">
+    <div className="container">
+      <Router>
+        <div className="nav-tittle">
+          <NavBar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Emoji />} />
+        </Routes>
+        <Footer {...footerinfo} />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Emoji.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Emoji = () => {
+  return (
+
+    <div>Emoji</div>
+  )
+}
+
+export default Emoji;
